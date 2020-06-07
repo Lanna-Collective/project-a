9fc69a9aa694fca57b934e09a7f1ef14d076723f
Lian Shao <lianshao2000@gmail.com>
2020-06-07 02:45:10 -0600

    added player movement, gravity, and fall-off-map detection

---
+++ GAME/Assets/Scripts/Respawn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+    public float threshold;
+    public Vector3 respawnPosition;
+
+    void FixedUpdate() {
+        if (transform.position.y < threshold)
+            transform.position = respawnPosition;
+    }
+}
