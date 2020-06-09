62426a29669111f06f0330d4fc3eb1263197cb00
Lian Shao <lianshao2000@gmail.com>
2020-06-09 03:56:45 -0600

    added shooting with raycasts, impact particles, crosshair, and a enemy with simple death

---
+++ GAME/Assets/Scripts/GunScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour {
+
+    public float damage = 10f;
+    public float range = 100f;
+    public ParticleSystem muzzleFlash;
+
+    public Camera playerCam;
+
+    void Shoot() {
+        RaycastHit hitInfo;
+        muzzleFlash.Play();
+
+        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hitInfo, range)) {
+            // Debug.Log(hitInfo.transform.name);
+
+            Shootable target = hitInfo.transform.GetComponent<Shootable>();
+            if (target != null) {
+                target.TakeDamage(damage);
+                GameObject impactGO = Instantiate(target.impactEffect, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+                Destroy(impactGO, 2f);
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            Shoot();
+        }
+
+    }
+}
---
+++ GAME/Assets/Scripts/Shootable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shootable : MonoBehaviour {
+    // Start is called before the first frame update
+
+    public float health = 50f;
+    public GameObject impactEffect;
+    public bool damageable;
+    public string objectType;
+
+    void Die(string deathBehavior) {
+        if (deathBehavior == "delete") {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void TakeDamage(float damage) {
+        if (!damageable) {
+            return;
+        }
+        health -= damage;
+        if (health <= 0) {
+            if (objectType == "human enemy")
+                Die("delete");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
